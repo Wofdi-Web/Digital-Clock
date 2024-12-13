372a0cb4d985fa5b316664b41c330ef9fa37af9f
ziyad aditya <ziyad.aditya.77@gmail.com>
2024-12-13 21:29:59 +0700

    Jam Selesai

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+// Digital Clock
+
+
+function getClock() {
+    const now = new Date();
+
+    //Hour
+    let hours = now.getHours();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    hours = hours %12 || 12;
+    hours = hours.toString().padStart(2, 0);
+
+    // Minutes
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+
+    // Seconds
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+
+    // Format
+    const timeString = `${hours} : ${minutes} : ${seconds} ${meridiem}`
+    document.getElementById("clock").textContent = timeString;
+};
+
+getClock();
+setInterval(getClock, 1000)
+
